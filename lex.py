6315ae0a93aa5dd3978f169dc45d74e6cb41bc52
ajabotomey <killerbyte_93@hotmail.com>
2024-01-16 22:34:06 +1000

    We can now read tokens

--- lex.py
+++ lex.py
@@ -1,3 +1,6 @@
+import enum
+import sys
+
 class Lexer:
     def __init__(self, source):
         self.source = source + "\n" # Source code to lex as a string
@@ -21,11 +24,12 @@ class Lexer:
 
     # Invalid token found, print error message and exit
     def abort(self, message):
-        pass
+        sys,exit("Lexing error. " + message)
 
     # Skip whitespace except newlines, which we will use to indicate the end of a statement
     def skipWhitespace(self):
-        pass
+        while self.curChar == ' ' or self.curChar == '\t' or self.curChar == '\r':
+            self.nextChar()
 
     # Skip comments in the code
     def skipComment(self):
@@ -33,4 +37,62 @@ class Lexer:
 
     # Return the next token
     def getToken(self):
-        pass
+        self.skipWhitespace()
+        token = None
+
+        # Check if the first character of this token to see if we can identify what it is
+        # If it is a multiple character operator (e.g., !=), number, identifier, or keyword then we will process the rest.
+        if self.curChar == '+':
+            token = Token(self.curChar, TokenType.PLUS)
+        elif self.curChar == '-':
+            token = Token(self.curChar, TokenType.MINUS)
+        elif self.curChar == '*':
+            token = Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar == '/':
+            token = Token(self.curChar, TokenType.SLASH)
+        elif self.curChar == '\n':
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == '\0': 
+            token = Token('', TokenType.EOF)
+        else:
+            # unknown token
+            self.abort("Unknown token: " + self.curChar)
+        
+        self.nextChar()
+        return token
+
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText; # The token's actual text. Used for identifiers, strings, and numbers.
+        self.kind = tokenKind; # The TokenType that this token is classified as.
+
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    # Keywords.
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDWHILE = 111
+    # Operators.
+    EQ = 201  
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
